e494737c7d9a31097b8a1579b48a2c2e76919210
Sjors Geelen <sjors.geelen@prorail.nl>
2026-01-19 06:02:59 +0000

    activate if present

--- src/app.rs
+++ src/app.rs
@@ -12,13 +12,22 @@ pub fn run() {
     // Record start time
     let _start_time = std::time::Instant::now();
 
+    use std::cell::RefCell;
+    use std::rc::Rc;
+    let window_ref: Rc<RefCell<Option<MainWindow>>> = Rc::new(RefCell::new(None));
     let app = Application::builder()
         .application_id("nl.dibitat.emoji_picker")
         .build();
 
+    let window_ref_clone = window_ref.clone();
     app.connect_activate(move |app| {
-        let window = MainWindow::new(app);
-        window.present();
+        let mut win = window_ref_clone.borrow_mut();
+        if win.is_none() {
+            *win = Some(MainWindow::new(app));
+        }
+        if let Some(ref window) = *win {
+            window.present();
+        }
     });
 
     app.run();
